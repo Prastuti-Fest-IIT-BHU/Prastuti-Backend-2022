1ba7c4c1c1223d9711a838ee2c032e856fec13ff
Nikhil Rana <nikhil.student.eee20@itbhu.ac.in>
2022-01-18 13:03:49 +0530

    Remove Conflict

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const Router = express.Router();
 
 const {getAllUsers, getUser, editUser, eventUser} = require('../controllers/user'); 
 const {getEventLeaderboard, getLeaderboard} = require('../controllers/leaderboard');
-const {addScore, addTeamScore} = require('../controllers/score');
+const {score_solo, score_team} = require('../controllers/score');
 const {deleteRequest, sendRequest, acceptRequest, getRequest} = require('../controllers/request.js');
 const {createTeam, getTeam} = require('../controllers/team');
 const { register_solo, register_team } = require('../controllers/register');
@@ -27,7 +27,7 @@ Router.route('/register/team').post(register_team);
 Router.route('/leaderboard').get(getLeaderboard);
 Router.route('/leaderboard/:event').get(getEventLeaderboard);
 
-Router.route('/score').post(addScore);
-Router.route('/score/team').post(addTeamScore);
+Router.route('/score').post(score_solo);
+Router.route('/score/team').post(score_team);
 
 module.exports = Router;
