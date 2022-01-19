ea24108c8921c38b676a09a05a0dcdaa164a0452
Shreyash Singh <shreyashsingh17@gmail.com>
2022-01-19 20:30:12 +0530

    add score controller

--- controllers/score.js
+++ controllers/score.js
@@ -4,14 +4,19 @@ const Team = require('../models/Teams');
 
 
 const score_solo = async (req,res) => {
-    
-    res.send("Working Path");
     const {score} = req.body;
-    const event = await Events.find({_id:req.body.event_id});
-    const user = await Users.find({_id:req.body.user_id});
+    const event = await Events.findById(req.body.event_id);
+    const user = await Users.findById(req.body.user_id);
 
+    if(!event || !user) {
+        res.status(404).json({
+            message: 'User or Event not found'
+        })
+        return;
+    }
     //Update total score in user
     user.Total_Score += score;
+    user.save();
 
     //Update score in event's model
     const participant_present = await event.Participants.find({_id: req.body.user_id});
