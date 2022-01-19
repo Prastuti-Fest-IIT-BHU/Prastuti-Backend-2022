812b9948516f2433835d829dc990d3c3b698b919
Shreyash Singh <shreyashsingh17@gmail.com>
2022-01-19 21:00:08 +0530

    add login controller

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 app.get('/test', (req, res) => {
     res.json({
--- controllers/login.js
+++ controllers/login.js
@@ -3,48 +3,44 @@ const res = require('express/lib/response');
 const {OAuth2Client} = require('google-auth-library');
 const User = require('../models/Users');
 
-const client = new OAuth2Client("/*client_id*/");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const Users = require('../models/Users');
 
 const loginUser = async (req, res) =>{
-    const {tokenId} = req.body;
+    const tokenId = req.body.tokenId;
+    let isNew = false; 
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: tokenId,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+        const payload = ticket.getPayload();
+        const curUser = await Users.findOne({
+            email_id: payload.email
+        })
 
-    let newUser = false; 
-
-    client.verifyIdToken({idToken: tokenId, audience: "/*client_id*/"}).then(response => {
-        const {email_verified, name, email, picture} = response.payload;
-
-        if(email_verified){
-            const user = Users.findOne({
-                Name: name,
-                email_id: email
-            });
-
-            if(user){
-                return res.json({
-                    user: user,
-                    newUser: newUser
-                });
+        if(!curUser) {
+            isNew = true;
+            let newUser = {
+                Name: payload.name,
+                email_id: payload.email,
+                Profile_Photo: payload.picture,
+                Teams: [],
+                Pending_Requests: [],
+                Events_Participated: []
             }
-            else {
-                newUser = true;
-
-                const user = Users.create({
-                    Name: name,
-                    email_id: email,
-                    Profile_Photo: picture,
-                }); 
-
-                return res.json({
-                    user: user,
-                    newUser: newUser
-                });
-            }
+            user = await Users.create(newUser);
         }
-        else{
-            return res.status(401).json({
-                message: 'email is not verified'
-            });
-        }        
+        else user = curUser;
+    }
+
+    verify().then(() => {
+        res.json({
+            message: 'Success',
+            user,
+            isNew
+        })
     })
 }
 
