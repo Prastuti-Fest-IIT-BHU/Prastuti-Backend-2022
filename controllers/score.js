bf7d1cd58f162df57850e4b3d9d0ff09ac6b470d
Shreyash Singh <shreyashsingh17@gmail.com>
2022-01-21 12:35:24 +0530

    add scoring routes

--- controllers/score.js
+++ controllers/score.js
@@ -2,35 +2,93 @@ const Users = require('../models/Users');
 const Events = require('../models/Events');
 const Team = require('../models/Teams');
 
-
 const score_solo = async (req,res) => {
     const {score} = req.body;
-    const event = await Events.findById(req.body.event_id);
-    const user = await Users.findById(req.body.user_id);
+    const event = await Events.findOne({ Name: req.body.event_name });
+    const user = await Users.findOne({ email_id: req.body.user_mail });
 
     if(!event || !user) {
         res.status(404).json({
+            status: 'Fail',
             message: 'User or Event not found'
         })
         return;
     }
+
+    if(event.Team_Event) {
+        res.json({
+            status: 'Fail',
+            message: 'This is a team event'
+        })
+        return;
+    }
+
+    const check = event.Participants.find(x => x.participant._id.equals(user._id));
+    if(!check) {
+        res.json({
+            status: 'Fail',
+            message: 'User not registered for this event'
+        })
+        return;
+    }
+
     //Update total score in user
     user.Total_Score += score;
     user.save();
 
-    //Update score in event's model
-    const participant_present = await event.Participants.find({_id: req.body.user_id});
+    // //Update score in event's model
+    let participant_present = event.Participants.find(x => x.participant._id.equals(user._id))
     participant_present.Score += score;
+    event.save();
+
+    res.json({
+        message: 'Score updated'
+    })
 }
 
 const score_team = async (req,res) => {
-    res.send("Working Path");
-    const { score } = req.body;
-    const team = await Team.find({_id:req.body.team_id});
+    const {score} = req.body;
+    const event = await Events.findOne({ Name: req.body.event_name });
+    const team = await Team.findOne({ slug: req.body.team_slug });
 
-    for(let i=0 ; i< team.Member_Count ; i++){
-        team.Members[i].Total_Score += score;
-    }   
+    if(!event || !team) {
+        res.status(404).json({
+            status: 'Fail',
+            message: 'Team or Event not found'
+        })
+        return;
+    }
+
+    if(!event.Team_Event) {
+        res.json({
+            status: 'Fail',
+            message: 'This is an individual event'
+        })
+        return;
+    }
+
+    const check = event.Teams.find(x => x.team._id.equals(team._id));
+    if(!check) {
+        res.json({
+            status: 'Fail',
+            message: 'Team not registered for this event'
+        })
+        return;
+    }
+
+    team.Members.forEach(async member => {
+        const curUser = await Users.findById(member._id);
+        curUser.Total_Score += score;
+        curUser.save();
+    })
+
+    let team_present = event.Teams.find(x => x.team._id.equals(team._id));
+    team_present.Score += score;
+    event.save();
+
+    res.json({
+        message: 'Score updated for team'
+    })
 }
 
 
